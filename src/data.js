37e3b3950f83e33de0f99a4f58d533cca67c687a
Loreto Gutiérrez <loreto.gutier.h@gmail.com>
2018-06-07 10:16:43 -0300

    Primer commit

---
+++ src/data.js
@@ -0,0 +1,17 @@
+//PARA CREAR FUNCIÓN GLOBAL
+window.computeUsersStats = (users, progress, courses) => {
+
+
+};
+
+window.sortUsers = (users, orderBy, orderDirection) => {
+
+};
+
+window.filterUsers = (users, filterBy) => {
+
+};
+
+window.processCohortData = ( ) => {
+
+};
