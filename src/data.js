b5f6a02df833fe26fa8eef3d9830ccefbf6f0e7f
Carolinadelb <carolinadelgadob@gmail.com>
2018-06-07 10:16:38 -0400

    este es para probar si funciona

---
+++ src/data.js
@@ -0,0 +1,16 @@
+window.computeUsersStats = (users, progress, courses) => {
+
+
+};
+
+window.sortUsers = (users, orderBy, ordedDirection) => {
+
+};
+
+window.filterUsers = (users, filterBy) => {
+
+};
+
+window.processCohortData = ( ) => {
+  
+}
