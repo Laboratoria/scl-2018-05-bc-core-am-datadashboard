c2afd145bbe52c83c84d23146f91921c024df82f
Mayra Romero <nahytar@gmail.com>
2018-06-07 10:41:46 -0400

    ajustes

---
+++ src/data.js
@@ -0,0 +1,11 @@
+window.computeUsersStats = (users, progress, courses) => {
+
+};
+
+window.sortUsers = (users, orderBy, orderDirection) => {
+
+};
+
+window.filterUsers = (users, search) => {
+
+};
