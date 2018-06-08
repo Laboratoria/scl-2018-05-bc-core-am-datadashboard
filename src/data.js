64a73ab58abccf1ba1f4f7b31b634260f9b604aa
valeria <valeria.penaloza5@gmail.com>
2018-06-08 13:01:20 -0400

    primer bosquejo de datadashboard

---
+++ src/data.js
@@ -0,0 +1,12 @@
+window.computeUsersStats = (users, progress, courses) =>{
+
+}
+window.sortUsers = (users, orderBy, orderDirection) =>{
+
+}
+window.filterUsers = (users, search) =>{
+
+}
+window.processCohortData = (options) =>{
+	
+}
