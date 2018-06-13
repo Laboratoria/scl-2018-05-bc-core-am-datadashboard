e53c598a39a390a35ce99758ccdbcb98d56e7e6a
Fanny <fannybecerravera@gmail.com>
2018-06-13 16:52:41 -0400

    Subiendo cambios de html css y grid haciendo el acordeon

--- src/data.js
+++ src/data.js
@@ -1,3 +1,34 @@
+// el primer for esta funcionando
+const acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        const content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+//este for no esta funcionando
+const sub1 = document.getElementsByClassName("submenu1");
+
+for (let i = 0; i < acc.length; i++) {
+    sub1[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        const content1 = this.nextElementSibling;
+        if (content1.style.display === "block") {
+            content1.style.display = "none";
+        } else {
+            content1.style.display = "block";
+        }
+    });
+}
+
+
+
 window.computeUsersStats = (users, progress, courses) => {
 
 };
