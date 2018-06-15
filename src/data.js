29e3ac27351d501a5d59ac714b58070e27189b62
TeresitaLee <teresita.1852@hotmail.com>
2018-06-15 13:46:01 -0400

    arreglando indentacion data.js

--- src/data.js
+++ src/data.js
@@ -1,17 +1,17 @@
-window.computeUsersStats = (users , progress , courses) => {
+window.computeUsersStats = (users, progress, courses) => {
 
 
 };
 
-window.sortUsers = (users, orderBy, orderDirection ) =>{
+window.sortUsers = (users, orderBy, orderDirection) =>{
 
 };
 
-window.filterUsers = (users, filterBy ) => {
+window.filterUsers = (users, filterBy) => {
 
 };
 
-window.processCohortData = (cohortData, orderBy, orderDirection, filterBy ) => {
+window.processCohortData = (cohortData, orderBy, orderDirection, filterBy) => {
 
 };
 
