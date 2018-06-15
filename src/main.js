3e1b2f0aede39eac8dd36863cbaecbea29db4c9c
Mayra Romero <nahytar@gmail.com>
2018-06-15 08:43:19 -0400

    se corrige desplegable de pestañas

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 const elements = document.getElementsByClassName('element');
 
-const toggle = (event) => {
-  if (event.currentTarget.getElementsByClassName('active').length === 0) {
-    event.currentTarget.children[1].classList.add('active'); 
-  } else if (event.currentTarget.getElementsByClassName('active').length === 1) {
-    event.currentTarget.children[1].classList.remove('active'); 
+const cleanActives = (element) => {
+  const activeChildren = element.getElementsByClassName('active');
+  for (let i = 0; i < activeChildren.length; i++) {
+    cleanActives(activeChildren[i]);
   }
+  element.classList.remove('active');
+}
+
+const toggle = (event) => {
+  let brothers = event.currentTarget.parentNode.children;
+  for (let i = 0; i < brothers.length; i++) {
+    let whiteCard = brothers[i].children[1];
+    if(brothers[i] !== event.currentTarget || whiteCard.classList.contains('active')) {
+      cleanActives(whiteCard);
+    } else {
+      event.currentTarget.children[1].classList.add('active');
+    }
+  }
+
+  event.stopPropagation();
 }
 
 for (let i = 0; i < elements.length; i++) {
