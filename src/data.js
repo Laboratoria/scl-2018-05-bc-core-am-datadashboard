ca5bffe9900918b6e78ab49515d151af1b184a19
valeria <valeria.penaloza5@gmail.com>
2018-06-14 21:44:11 -0400

    modificacion maquetacion

--- src/data.js
+++ src/data.js
@@ -1,6 +1,14 @@
+/*crea una lista de estudiantes*/
+/*users: Arreglo de objetos obtenido de la data en bruto.*/
+/*progress: Objeto de progreso en bruto. Contiene una llave para cada usuario (uid) con 
+un objeto que contiene el progreso del usuario para cada curso. */ 
+/*courses: Arreglo de strings con los ids de los cursos del cohort en cuestión. 
+Esta data se puede extraer de la propiedad coursesIndex de los objetos que 
+representan los cohorts. */
 window.computeUsersStats = (users, progress, courses) =>{
 
 }
+/**/
 window.sortUsers = (users, orderBy, orderDirection) =>{
 
 }
