82941621ebfd21d9f2f70eda90430e9949b6f54b
Karyn Herrera <karynherrera@gmail.com>
2018-06-18 14:32:05 -0400

    Actualizacion navbar collapse funcional

--- src/main.js
+++ src/main.js
@@ -16,6 +16,27 @@ btnAlumna.addEventListener('click', function(event){
 
 btnCursos.addEventListener('click', function(event){
   event.preventDefault();
- return seccionAlumna.style.display='inline';
+ return seccionAlumna.style.display='block';
 });
 
+//Haciendo conexion a JSON
+const btn = document.getElementById('conect');
+const container = document.getElementById('root');
+const usersJSON='../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+
+fetch(usersJSON)
+.then(response => response.json())
+.then(data =>{
+	console.log(data);
+	renderUsers(data);
+})
+
+const renderUsers = data =>{ //for in si fuera un arreglo de objetos
+	btn.addEventListener('click',()=>{
+		const render = data.forEach(element =>{
+			//element.name === arrelo[i].name
+			return container.innerHTML += `<p>${element.name}</p>`
+		})
+	return render;
+	});
+}
