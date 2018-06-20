3abaa6e52c0da43f50ff0540b2dad27f7cdc0193
valeria <valeria.penaloza5@gmail.com>
2018-06-19 20:48:50 -0400

    modificando logica de data.js

--- src/data.js
+++ src/data.js
@@ -1,57 +1,50 @@
-window.computeUsersStats = (users, progress, courses) =>{
-// const btnNames = document.getElementById('btnUsers');
-// const containerNames = document.getElementById('root');
-// const btnCourseUsers = document.getElementById('btnCourse');
-// const containerCourse = document.getElementById('root2');
-// //guarda la ruta del archivo
-// const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
-// //hace la consulta de json
-// fetch(usersJSON)
-// //guarda una promesa de si se cumple
-// .then(response => response.json())
-// //recibe la respues de lo consultado
-// .then(data => {
-// 	console.log(data);
-// 	renderUsers(data);
-// })
-// renderUsers = data => {
-// 	btnNames.addEventListener('click', () => {
-// 		const render = data.forEach(element => {
-// 			return containerNames.innerHTML += `<p>Nombre: ${element.name}</p>` 
-// 		})
-// 		return render;
-// 	})
-// }
-
-
-// //guarda la ruta del archivo
-// const coursesJSON = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
-// //hace la consulta de json
-// fetch(coursesJSON)
-// //guarda una promesa de si se cumple
-// .then(responseCourse => responseCourse.json())
-// //recibe la respues de lo consultado
-// .then(cursos => {
-// 	console.log(cursos);
-// 	renderCourse(cursos);
-// })
-// renderCourse = cursos => {
-// 	btnCourseUsers.addEventListener('click', () => {
-// 		console.log(cursos);
-// 		for(let x in cursos){
-// 			console.log("objeto es = "+x);
-// 		}
-// 	})
-// }
-
+//CARGA USER.JSON (ALUMNAS)
+//funcionalidad del btn
+document.getElementById('btnAlumnas').addEventListener('click', cargarAlumnas);
+//llamando la funcion
+function cargarAlumnas() {
+	fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json')
+	.then(function(resAlumnas){
+		//retorna el valor en formato json
+		return resAlumnas.json();
+	}).then(function(alumnas){
+		let html = '';
+		alumnas.forEach(function(alumnasId){
+			html += `<p>Nombre Alumna: ${alumnasId.name}</p> <p>Id Alumna: ${alumnasId.id}</p> <p>Rol: ${alumnasId.role}</p>`;
+		})
+		document.getElementById('resultado').innerHTML = html;
+	})
 }
 
-window.sortUsers = (users, orderBy, orderDirection) =>{
+//CARGA PROGRESS.JSON (CURSOS)
+document.getElementById('btnCursos').addEventListener('click', cargarCursos);
+function cargarCursos(){
+	fetch('../data/cohorts/lim-2018-03-pre-core-pw/progress.json')
+	.then(function(resCursos){
+		return resCursos.json();
+	})
+	.then(function(cursosProgress){
+		console.log(cursosProgress);
+		// let html = '';
+		// cursosProgress.forEach(function(cursos){
+		// 	html += `<p>Intro : ${cursos.intro}</p>`;
+		// })
+		// document.getElementById('resultado').innerHTML = html;
+	})
+}
 
+
+
+
+
+
+
+
+window.computeUsersStats = (users, progress, courses) =>{
+}
+window.sortUsers = (users, orderBy, orderDirection) =>{
 }
 window.filterUsers = (users, search) =>{
-
 }
 window.processCohortData = (options) =>{
-	
 }
