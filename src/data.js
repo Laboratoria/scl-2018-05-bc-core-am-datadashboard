3544e691fb30c6e265345edee1442cf6a96544c5
valeria <valeria.penaloza5@gmail.com>
2018-06-20 14:58:50 -0400

    pushando arreglos

--- src/data.js
+++ src/data.js
@@ -1,31 +1,50 @@
+//guardando en variables las url
 const usersJson = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
 const progressJson = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
 const cohortsJson = '../data/cohorts.json';
-
+//guardando las llamadas
 var usersData = fetch(usersJson)
 .then(response => response.json())
 .then(users => {
-	console.log(users);
 	usersData = users;
 	recolector();
+	nameUsers(users);
 });
+//guardando en un arreglo y pushando
+const nameUsers = (users) =>{
+	let usersArray = [];
+	users.forEach(element => {
+		usersArray.push(element.name);
+	});
+	console.log(usersArray);
+}
 
 var progressData = fetch(progressJson)
 .then(response => response.json())
 .then(progressJson => {
 	const progress = Object.entries(progressJson);
-	console.log(progress);
 	progressData = progress;
 	recolector();
 });
 
+
 var cohortsData = fetch(cohortsJson)
 .then(response => response.json())
 .then(courses => {
-	console.log(courses);
 	cohortsData = courses;
 	recolector();
+	nameCourse(courses);
 });
+
+const nameCourse = (courses) =>{
+	let coursesArray = [];
+	courses.forEach(element => {
+		coursesArray.push(element.coursesIndex);
+	});
+	console.log(coursesArray);
+}
+
+
 
 function recolector(){
 	if(usersData && progressData && cohortsData){
