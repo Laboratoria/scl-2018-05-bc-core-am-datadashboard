b0fc25e8eb979f04a66f9a76e0d9b5897a57d092
Carolinadelb <carolinadelgadob@gmail.com>
2018-06-20 12:42:08 -0400

    tratando de hacer un buscador

--- src/data.js
+++ src/data.js
@@ -3,6 +3,7 @@ const studentList = document.getElementById('names');
 const container = document.getElementById('studentListBox');
 const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
 
+
 fetch(usersJSON)
 //esto nunca cambia
 .then(response => response.json())
@@ -18,11 +19,43 @@ const renderUsers = data => {
     //element representa cada uno de los elementos del arreglo
     const render = data.forEach(element => {
       //para imprimir todos los nombres dentro del container (root) de html, uno tras otro
-      return container.innerHTML += `<p>${element.name}</p>`;
+      return container.innerHTML += `<p>${element.name.toUpperCase()}</p>`;
     })
     return render;
   });
 }
+
+const inputText = document.querySelector('input');
+const containerStudent = document.getElementById('studentSearch');
+
+inputText.addEventListener('keypress',(event) => {
+  let key = event.which || event.keyCode; //which keyCodo son método que saca el código de la tecla
+  if (key === 13) { //código 13 es enter
+    let nameStudent = inputText.value; //punto value siempre con let
+    inputText.value = '';
+    const render = data.forEach(element => {
+      //para imprimir todos los nombres dentro del container (root) de html, uno tras otro
+      return container.innerHTML += `<p>${element.name.toUpperCase()}</p>`;
+    })
+    return render;
+    console.log(nameStudent);
+    //URL de api, más el key mandaron por correo:
+    fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      renderInfo(data);
+
+    })
+  }
+})
+
+const renderInfo = (data) => {
+  containerStudent.innerHTML = data.name;
+} 
+
+
+
 
 
 
