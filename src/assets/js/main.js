a2b9d76002102191348d1a6a44c01ba57ee56141
mariajosesalazar <mariaj.salazarf@gmail.com>
2018-06-20 13:04:35 -0400

    display's none funcionales, falta conectar con cada boton correspondiente

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,34 +1,43 @@
 
 window.onload = () => {
-	
+
 	//llamando al JSON con fetch
 	const cohortsJSON = '../data/cohorts.json';
 	fetch(cohortsJSON) 
 	.then(response => response.json())
 	.then(data => { 
-	  
+		
 		cohortsFull(data);
 	})
 	
 	const cohortsFull = data => {
-	let cohortsID = [];
-	data.forEach(elem => {
-	//comparar id para sacar solo los de lima con  if 	
-		if (elem.sede === "lima"){
-	//mostrar id de esos cohort	
-		cohortsID.push(elem.id);
-	 	} 
-	});
-	//transforme a texto los ids 
-	let cohortsIdText = JSON.stringify(cohortsID);
-	
-	//mostrar id de esos cohort y listarlos con innerHTML  
-	/*document.getElementById("showCohortList").innerHTML = cohortsIdText; */
+		let cohortsID = [];
+		data.forEach(elem => {
+			//comparar id para sacar solo los de lima con  if 	
+			if (elem.sede === "lima"){
+				//mostrar id de esos cohort	
+				cohortsID.push(elem.id);
+			} 
+		});
+		//transforme a texto los ids 
+		let cohortsIdText = JSON.stringify(cohortsID);
+		
+		//mostrar id de esos cohort y listarlos con innerHTML  
+		/*document.getElementById("showCohortList").innerHTML = cohortsIdText; */
 	}
-
+	
 }
 
+//seccion botones
+//area de busqueda
+const btnSearch = document.getElementById('btnSearch').addEventListener('click', () => {
+	const searchA = document.getElementById('searchAlumnas').value;
+	//aqui deberia ir la funcion que permite imprimir el nombre de las alumnas
+	console.log(searchA);
+	document.getElementById('cohorts').style.display = 'block';
+	console.log(cohortsIdText);
+	
+})
 
-
-
-
+//
+const cohortLima = cohortsIdText.innerHTML;
