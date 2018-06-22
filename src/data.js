dc9b5aef714c7ae48983a440fc7e88ea0dbc63e5
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-06-21 20:58:01 -0400

    agreguada propiedad stats a users

--- src/data.js
+++ src/data.js
@@ -2,52 +2,50 @@ const userJson = "http://127.0.0.1:8887/data/cohorts/lim-2018-03-pre-core-pw/use
 const progressJson = "http://127.0.0.1:8887/data/cohorts/lim-2018-03-pre-core-pw/progress.json"
 const cohortJson = "http://127.0.0.1:8887/data/cohorts.json"
 
-
-window.data = {//variable declarada como un objeto con 1 prop. contex q es un objeto
+window.data = {//variable declarada como un objeto con 2 prop. loadin y contex q es un objeto
+  loading: 3,//se tienen q cumplir las 3 promesas fetch, a medida q una se cumpla se le restará 1
   context: {
     cohorts: null,
     users: null,
     progresses: null,
-    cohortDatas: null
   }
 };
 
+window.loadData = (loadComplete) => {//metodo loadData q recibe una función y ejecuta los 3 fetch
   fetch(cohortJson)
     .then(response => response.json())
     .then(data => {
-      window.data.context.cohorts = data;
-      stopLoading();
+      window.data.context.cohorts = data;//le asigno el valor de data a cohort
+      window.data.loading--;//cada vez q se cumple un fetch le disminuye uno a la var loading q está dentro de data, dentro de windows
+
+      if (window.data.loading == 0) completePromise(loadComplete);//ya finalizaron y llama a loadComplete
     });
 
   fetch(userJson)
     .then(response => response.json())
     .then(data => {
       window.data.context.users = data;
-      stopLoading();
+      window.data.loading--;
+
+      if (window.data.loading == 0) completePromise(loadComplete);
     });
 
   fetch(progressJson)
     .then(response => response.json())
     .then(data => {
-      window.data.context.progress = data;
-      stopLoading();
+      window.data.context.progresses = data;
+      window.data.loading--;
+
+      if (window.data.loading == 0) completePromise(loadComplete);
     });
 
-  const stopLoading = () => { //función que sincroniza el término de cada promesa...
-    if (window.data.context.users && window.data.context.users && window.data.context.users) {//Cuando ya no tienen valor null
-      // Termine de cargar
-
-      loadCompleteUsers();
-    }
+  var completePromise = (loadComplete) => {
+      (window.data.context.users,
+      window.data.context.progresses,
+      window.data.context.cohorts
+    );
+    loadComplete(window.data.context);
   };
-
-const loadCompleteUsers = () => {
-  cohortBtn.addEventListener("click", () => {
-    const render = window.data.context.users.forEach(element => {
-      return contentDiv.innerHTML += `<p>${element.name}</p>`
-    })
-    return render;
-  })
 };
 
 window.getCohortUsers = (users, cohort) => {//seleccion de usuarios por cohort
@@ -67,11 +65,23 @@ window.getCohortUsers = (users, cohort) => {//seleccion de usuarios por cohort
 
 
 
-window.computeUsersStats = (users, progress, courses) => {
+window.computeUsersStats = (users, progresses, courses) => {//Las llaves de los cursos se obtienen de la prop coursesIndex
+  var result = [];
 
+  if (users && progresses && courses) {//Validación
+    for (var i = 0; i < users.length; i++) {
+      var user = users[i];//cada usuario
+      var progress = progresses[user.id];//por cada usuario saco su progreso, indexo en [] el id del usuario en la var progreso.
+
+      user.stats = {//creo la prop. stats en usuarios  
+        percent: 0,
+        exercises: { total: 0, completed: 0, percent: 0 },
+        reads: { total: 0, completed: 0, percent: 0 },
+        quizzes: { total: 0, completed: 0, percent: 0, scoreSum: 0, scoreAvg: 0 },
+      };
+    };
+  };  
 };
-
-
 
 window.sortUsers = (users, orderBy, orderDirection) => {
 
--- src/main.js
+++ src/main.js
@@ -3,3 +3,8 @@ const cohortBtn = document.getElementById("cohortBtn");
 
 //creé las variables que utilizaremos como los objetos, aun no tienen valor puesto que aún no se cumple 
 //la promesa que nos devolverá el JSON como datos
+window.loadData(data => {//data devuelve el "contexto" de datos; cohort, users, datos
+  
+  loading.style.display = 'none';
+  
+});
